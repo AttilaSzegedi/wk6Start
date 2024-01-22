2d33f99c208816bbbe81d03daaac04e02678e6c0
AttilaSzegedi <151849683+AttilaSzegedi@users.noreply.github.com>
2024-01-22 20:47:21 +0100

    Change component name
    change my-element to webuni-card

--- docs-src/examples/index.md
+++ docs-src/examples/index.md
@@ -1,20 +1,20 @@
 ---
 layout: example.11ty.cjs
-title: <my-element> ⌲ Examples ⌲ Basic
+title: <webuni-card> ⌲ Examples ⌲ Basic
 tags: example
 name: Basic
 description: A basic example
 ---
 
 <style>
-  my-element p {
+  webuni-card p {
     border: solid 1px blue;
     padding: 8px;
   }
 </style>
-<my-element>
+<webuni-card>
   <p>This is child content</p>
-</my-element>
+</webuni-card>
 
 <h3>CSS</h3>
 
@@ -28,7 +28,7 @@ p {
 <h3>HTML</h3>
 
 ```html
-<my-element>
+<webuni-card>
   <p>This is child content</p>
-</my-element>
+</webuni-card>
 ```
--- test/my-element_test.js
+++ test/my-element_test.js
@@ -4,18 +4,18 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import {MyElement} from '../my-element.js';
+import {WebuniCard} from '../webuni-card.js';
 import {fixture, assert} from '@open-wc/testing';
 import {html} from 'lit/static-html.js';
 
-suite('my-element', () => {
+suite('webuni-card', () => {
   test('is defined', () => {
-    const el = document.createElement('my-element');
-    assert.instanceOf(el, MyElement);
+    const el = document.createElement('webuni-card');
+    assert.instanceOf(el, WebuniCard);
   });
 
   test('renders with default values', async () => {
-    const el = await fixture(html`<my-element></my-element>`);
+    const el = await fixture(html`<webuni-card></webuni-card>`);
     assert.shadowDom.equal(
       el,
       `
@@ -27,7 +27,7 @@ suite('my-element', () => {
   });
 
   test('renders with a set name', async () => {
-    const el = await fixture(html`<my-element name="Test"></my-element>`);
+    const el = await fixture(html`<webuni-card name="Test"></webuni-card>`);
     assert.shadowDom.equal(
       el,
       `
@@ -39,7 +39,7 @@ suite('my-element', () => {
   });
 
   test('handles a click', async () => {
-    const el = await fixture(html`<my-element></my-element>`);
+    const el = await fixture(html`<webuni-card></webuni-card>`);
     const button = el.shadowRoot.querySelector('button');
     button.click();
     await el.updateComplete;
@@ -54,7 +54,7 @@ suite('my-element', () => {
   });
 
   test('styling applied', async () => {
-    const el = await fixture(html`<my-element></my-element>`);
+    const el = await fixture(html`<webuni-card></webuni-card>`);
     await el.updateComplete;
     assert.equal(getComputedStyle(el).paddingTop, '16px');
   });
